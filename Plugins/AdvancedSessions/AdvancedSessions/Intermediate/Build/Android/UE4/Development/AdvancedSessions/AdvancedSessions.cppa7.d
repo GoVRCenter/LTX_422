ec57db48e3d391710a5bb6e412669d9958437c31
Ham1ton <Justin@tonofham.com>
2019-10-23 17:04:38 -0400

    Updated Tron Guy

--- Plugins/AdvancedSessions/AdvancedSessions/Intermediate/Build/Android/UE4/Development/AdvancedSessions/AdvancedSessions.cppa7.d
+++
@@ -1,5 +0,0 @@
-C:/Users/Justin/Documents/Unreal\ Projects/LTX_422/Plugins/AdvancedSessions/AdvancedSessions/Intermediate/Build/Android/UE4/Development/AdvancedSessions/AdvancedSessions.cppa7.o: \
-  C:/Users/Justin/Documents/Unreal\ Projects/LTX_422/Plugins/AdvancedSessions/AdvancedSessions/Source/AdvancedSessions/Private/AdvancedSessions.cpp \
-  C:/Users/Justin/Documents/Unreal\ Projects/LTX_422/Intermediate/Build/Android/LazerTagX/Development/Engine/SharedPCH.Engine.h \
-  C:/Users/Justin/Documents/Unreal\ Projects/LTX_422/Plugins/AdvancedSessions/AdvancedSessions/Intermediate/Build/Android/UE4/Development/AdvancedSessions/Definitions.AdvancedSessions.h \
-  C:/Users/Justin/Documents/Unreal\ Projects/LTX_422/Plugins/AdvancedSessions/AdvancedSessions/Source/AdvancedSessions/Classes\AdvancedSessions.h
